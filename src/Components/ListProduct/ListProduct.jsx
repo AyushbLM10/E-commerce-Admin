3327d42b7cd27f9f277566866f1a8a90150e4de1
AyushbLM10 <ayushbhattab65@gmail.com>
2024-05-10 07:43:30 +0530

    admin

--- src/Components/ListProduct/ListProduct.jsx
+++ src/Components/ListProduct/ListProduct.jsx
@@ -6,7 +6,7 @@ const ListProduct = () => {
   const [allproducts,setAllProducts] = useState([]);
 
   const fetchInfo = async ()=>{
-    await fetch('http://localhost:4000/allproducts')
+    await fetch('https://e-commerce-backend-ruby-eight.vercel.app/allproducts')
         .then((res)=>
         res.json()).then((data)=>{setAllProducts(data)});
   }
@@ -16,7 +16,7 @@ const ListProduct = () => {
   },[])
 
   const remove_product = async (id)=>{
-    await fetch('http://localhost:4000/removeproducts',{
+    await fetch('https://e-commerce-backend-ruby-eight.vercel.app/removeproducts',{
       method:'POST',
       headers:{
         Accept: 'application/json',// It means client is expecting json data
